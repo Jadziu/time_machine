48c579f4b2cae83254d244215382107d7a41f52c
Paweł Jagowdzik <paweljagowdzik@gmail.com>
2023-02-23 00:03:06 +0100

    Update time_machine.ino
    new almost everything

--- time_machine.ino
+++ time_machine.ino
@@ -1,28 +1,32 @@
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>,
+#include <hd44780.h>                      
+#include <hd44780ioClass/hd44780_I2Cexp.h>
 #include <Adafruit_NeoPixel.h>
 #include <EEPROM.h>
 
 #define PIN 10
-#define NUMPIXELS 16
+#define NUMPIXELS 8
 
-LiquidCrystal_I2C lcd(0x27,20,4);
+hd44780_I2Cexp lcd;
+hd44780_I2Cexp lcd2(0x27);
+
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-long record = 9999;
+uint16_t record = 9999;
 long start = 0;
 long lastTime = 0;
 long reaction = 0;
 const int trig = 3;
 const int reset = 4;
-const int led = 13;
+const int LCD_COLS = 16;
+const int LCD_ROWS = 2;
 int buttonState = 0;
 int button2State = 0;
 long lastMillis = 0;
 int rectime = 3000;
 int state = 0;
-int t=0;
-int col_sat = 250;
+int t = 0;
+int col_sat = 50;
 uint16_t now_record;
 byte record_hi;
 byte record_lo;
@@ -52,74 +56,55 @@ byte Blank[] = {
 
 void setup()
 {
-  pinMode(led, OUTPUT);
-  Serial.begin(115200);
-  delay(3000);
-  Serial.println("Hello, I'm DEBUG.");
-  Serial.println(" ");
+  pixels.begin();
+  pixels.clear();
+  pixels.show();
+  delay(1000);
+
   button2State = digitalRead(reset);
-  delay(3000);
+
+  //sprawdzenie zawartości EEPROM
   record_hi = EEPROM.read(0);
   record_lo = EEPROM.read(1);
-  Serial.println("values in eeprom 8bit: ");
-  Serial.print("hi: ");
-  Serial.println(record_hi,HEX);
-  Serial.print("lo: ");
-  Serial.println(record_lo,HEX);
 
-  now_record = (record_hi <<8) + record_lo;
-  Serial.println("values in eeprom 16bit: ");
-  Serial.print("now_record: ");
-  Serial.println(now_record);
-  Serial.println(" ");
+  now_record = (record_hi << 8) + record_lo;
 
-  delay(3000);
+  delay(500);
+
+  //Wyświetlenie informacji o nie prawidłowej lub resecie wartości EEPROM.
   if (now_record > 9999 || now_record == 0 || button2State == HIGH){
     if (button2State == HIGH){
-      Serial.println("RESETING RECORD TO DEFAULT!");
-      for (int i = 0; i <= 10; i++){
-        digitalWrite(led, HIGH);
+      for (int i=0; i<=10; i++){
+        pixels.setPixelColor((0), pixels.Color(250, 250, 250));
+        pixels.show();
         delay(150);
-        digitalWrite(led, LOW);
+        pixels.clear();
+        pixels.show();
         delay(150);
       }
     }
-    Serial.print("Write new record to EEPROM. HIGH: ");
-    digitalWrite(led, HIGH);
+
+    //Wpisanie wartośći początkowej do EEPROM (9999).
     record_hi = record >> 8;
     record_lo = record & 0x00FF;
-    EEPROM.write(record_hi, 0);
-    EEPROM.write(record_lo, 1);
-
-    Serial.print(record_hi,HEX);
-    Serial.print(" LOW: ");
-    Serial.print(record_lo,HEX);
-    Serial.println(" ");
+    //Zapis do EEPROM.
+    EEPROM.write(0, record_hi);
+    EEPROM.write(1, record_lo);
+    pixels.setPixelColor((0), pixels.Color(250, 250, 250));
+    pixels.show();
     delay(1000);
-    digitalWrite(led, LOW);
+    pixels.clear();
+    pixels.show();
   }
 
-  delay(3000);
-
+  //Ponowny odczyt rekordu z pamięci
   record_hi = EEPROM.read(0);
   record_lo = EEPROM.read(1);
-  Serial.println("values in eeprom 8bit: ");
-  Serial.print("hi: ");
-  Serial.println(record_hi);
-  Serial.print("lo: ");
-  Serial.println(record_lo);
 
-  now_record = (record_hi <<8) + record_lo;
-  Serial.println("values in eeprom 16bit: ");
-  Serial.print("now_record: ");
-  Serial.println(now_record);
-  Serial.println(" ");
+  //Wpisanie liczby z EEPROM do zmiennej RECORD.
+  record = (record_hi << 8) + record_lo;
 
-  delay(10000);
-
-  Serial.println("end");
-
-
+  //inicjalizacja przerwania.
   cli();          
   TCCR1A = 0;     
   TCCR1B = 0;     
@@ -128,26 +113,21 @@ void setup()
   OCR1A = 31250;
   sei();
 
+
   pinMode(trig, INPUT);
-  
-  lcd.init();                      
-  lcd.init();
+  lcd.begin(LCD_COLS, LCD_ROWS);              
   lcd.createChar(0, Block);
   lcd.createChar(1, Blank);
   lcd.backlight();
-  
-  pixels.begin();
-  pixels.clear();
-  pixels.show();
-
   lcd.clear();
   lcd.setCursor(4,0);
   lcd.print("MADE BY:");
   lcd.setCursor(3,1);
   lcd.print("@PJ_MAKER");
   delay(1000);
-  
-  for(int i=0;i<=16;i++){
+
+  //Animacja na led.
+  for(int i=0; i<=16; i++){
     lcd.setCursor(i,0);
     lcd.write(0);
     lcd.setCursor(i,1);
@@ -156,7 +136,7 @@ void setup()
     pixels.show();
     delay(50);
   }
-  Serial.println("LAUNCHING...");
+
   delay(3000);
   lcd.clear();
   lcd.setCursor(1,0);
@@ -167,41 +147,35 @@ void setup()
   lcd.clear();
 }
 
-
 void Ready(){
   lcd.setCursor(5,0);
   lcd.print("READY?");
-  Serial.println("Ready to  ROCK!!");
 }
 
 void Times(){
-  if(state > 1) state =0;
+  if(state > 1) state = 0;
   if(state == 0){
     lcd.print("RECORD: ");
     lcd.print(record);
     lcd.print("ms");
-    lcd.write(1);
-    lcd.write(1);
-    lcd.write(1);
-    lcd.write(1);
-    lcd.write(1);
+    for (int j=0; j<4; j++)
+    {
+      lcd.write(1);
+    }
   }
   else{  
     lcd.print("LAST:   ");
     lcd.print(lastTime);
     lcd.print("ms");
-    lcd.write(1);
-    lcd.write(1);
-    lcd.write(1);
-    lcd.write(1);
-    lcd.write(1);
-    
+    for (int k=0; k<4; k++)
+    {
+      lcd.write(1);
+    }
   }
-  
 }
 
 void clearLine(int l){
-  for(int i=0;i<16;i++){
+  for(int i=0; i<16; i++){
     lcd.setCursor(i,l);
     lcd.write(1);
     lcd.setCursor(0,1);
@@ -209,10 +183,10 @@ void clearLine(int l){
 }
 
 void loop() {
-  lcd.backlight(); 
+  lcd.backlight();
   
-  for(int j=0; j<=15;j++){
-    pixels.setPixelColor(j, pixels.Color(col_sat, 0, 0));
+  for(int m=0; m<=15; m++){
+    pixels.setPixelColor(m, pixels.Color(col_sat, 0, 0));
     pixels.show();
   }
 
@@ -223,24 +197,22 @@ void loop() {
   t=0;
 
   while (buttonState == HIGH){
-    Serial.println("Waiting for clutch relase");
-    for(t;t<1;t++){
+    for(t; t<1; t++){
       lcd.clear();
       lcd.noBacklight();
       lastTime++;
-      start = random(1000, 4000);
-      Serial.println(start);
+      start = random(500, 3500);
     }
 
-    for(int j=0; j<=15;j++){
-      pixels.setPixelColor(j, pixels.Color(col_sat, 0, 0));
+    for(int n=0; n<=8; n++){
+      pixels.setPixelColor(n, pixels.Color(col_sat, 0, 0));
       pixels.show();
     }
     
     delay(1500);
    
-    for(int j=0; j<=15;j++){
-      pixels.setPixelColor(j, pixels.Color(0, col_sat, 0));
+    for(int o=0; o<=8; o++){
+      pixels.setPixelColor(o, pixels.Color(0, col_sat, 0));
       pixels.show();
     }
     
@@ -266,27 +238,32 @@ void loop() {
     }
     
     reaction = millis() - lastMillis;
+
     lcd.backlight();
     lcd.setCursor(0,0);
     lcd.print("YOUR TIME:");
     lcd.setCursor(0,1);
     lcd.print(reaction);
     lcd.print("ms");
+
     lastTime = reaction;
+
     if(reaction < record){
       record = reaction;
+      record_hi = record >> 8;
+      record_lo = record & 0x00FF;
+      EEPROM.write(0, record_hi);
+      EEPROM.write(1, record_lo);
       lcd.setCursor(0,0);
       lcd.print("NEW RECORD TIME:");
-      for(int h=0;h<=6;h++){
-        for(int j=0; j<=15;j++){
+      for(int h=0; h<=6; h++){
+        for(int j=0; j<=8; j++){
           pixels.setPixelColor(j, pixels.Color(col_sat, col_sat, 0));
           pixels.show();
         }
         delay(500);
-        for(int j=0; j<=15;j++){
-          pixels.setPixelColor(j, pixels.Color(0, 0, 0));
-          pixels.show();
-        }
+        pixels.clear();
+        pixels.show();
         delay(500);
        }       
      }
